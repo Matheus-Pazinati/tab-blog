89ceaf5958d9da6cce31cb855ddd605db9536a7d
Matheus Pazinati <matheuspazinati@gmail.com>
2023-01-17 16:45:49 -0300

    Configura o styled-components no projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalReset } from "./styles/global";
+import { defaulTheme } from "./styles/themes/default";
+
 export function App() {
 
-  return <h1>Tab Blog</h1>
+  return (
+    <ThemeProvider theme={defaulTheme}>
+      <GlobalReset />
+      <h1>Tab Blog</h1>
+    </ThemeProvider>
+  )
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,12 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+export const GlobalReset = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    font-family: 'Nunito', sans-serif;
+  }
+`
