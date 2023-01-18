3aa2df81bde1c10e006f0d993a63a924e0bf55fa
Matheus Pazinati <matheuspazinati@gmail.com>
2023-01-18 08:18:42 -0300

    Adiciona eslint ao projeto para padronizar a escrita do codigo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { ThemeProvider } from "styled-components";
-import { GlobalReset } from "./styles/global";
-import { defaulTheme } from "./styles/themes/default";
+import { ThemeProvider } from 'styled-components'
+import { GlobalReset } from './styles/global'
+import { defaulTheme } from './styles/themes/default'
+
+import { Home } from './pages/Home'
 
 export function App() {
-
   return (
     <ThemeProvider theme={defaulTheme}>
       <GlobalReset />
-      <h1>Tab Blog</h1>
+      <Home />
     </ThemeProvider>
   )
 }
