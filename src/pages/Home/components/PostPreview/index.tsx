54dfc59d429e37329ce912fdd4004fba8e0de5bd
Matheus Pazinati <matheuspazinati@gmail.com>
2023-01-23 18:15:33 -0300

    Cria estrutura do header dos posts

---
+++ src/pages/Home/components/PostPreview/index.tsx
@@ -0,0 +1,11 @@
+export function PostPreview() {
+  return (
+    <main>
+      <div>
+        <h2>Publicações</h2>
+        <span>6 publicações</span>
+      </div>
+      <input type="text" placeholder="Buscar conteúdo" />
+    </main>
+  )
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,11 @@
+import { PostPreview } from './components/PostPreview'
 import { Profile } from './components/Profile'
 
 export function Home() {
-  return <Profile />
+  return (
+    <>
+      <Profile />
+      <PostPreview />
+    </>
+  )
 }
