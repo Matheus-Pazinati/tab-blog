cdc9c1c8d44a295729a2048b232e4183831bca82
Matheus Pazinati <matheuspazinati@gmail.com>
2023-02-04 11:01:43 -0300

    Busca dados do Github e preenche o perfil do blog

--- src/pages/Home/components/Profile/index.tsx
+++ src/pages/Home/components/Profile/index.tsx
@@ -6,37 +6,60 @@ import {
 } from '@fortawesome/free-solid-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { ProfileContainer, ProfileIcon } from './styles'
+import { useEffect, useState } from 'react'
+import { githubApi } from '../../../../lib/api'
+
+interface ProfileTypes {
+  name: string
+  bio: string
+  login: string
+  avatar_url: string
+  html_url: string
+  followers: string
+}
 
 export function Profile() {
+  const [profile, setProfile] = useState<ProfileTypes>({
+    name: '',
+    bio: '',
+    login: '',
+    avatar_url: '',
+    html_url: '',
+    followers: '',
+  })
+
+  async function getGithubProfile() {
+    const response = await githubApi.get('/Matheus-Pazinati')
+    const data = (await response.data) as ProfileTypes
+
+    setProfile(data)
+  }
+
+  useEffect(() => {
+    getGithubProfile()
+  }, [])
   return (
     <ProfileContainer>
       <div className="ProfileContent">
         <img
-          src="https://rollingstone.uol.com.br/media/_versions/keanu_reeves_como_neo_em_matrix_divulgacao_widelg.jpg"
-          alt=""
+          src={profile.avatar_url}
+          alt={`Foto do perfil do Github do usuário ${profile.name}`}
         />
         <div className="ProfileData">
           <div className="ProfileName">
-            <h1>Matheus Pazinati</h1>
-            <a
-              href="https://www.github.com/Matheus-Pazinati"
-              target={'_blank'}
-              rel="noreferrer"
-            >
+            <h1>{profile.name}</h1>
+            <a href={profile.html_url} target={'_blank'} rel="noreferrer">
               <span>
                 Github
                 <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
               </span>
             </a>
           </div>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab,
-            temporibus? Officiis expedita magni quisquam qui.
-          </p>
+          <p>{profile.bio}</p>
           <ul className="ProfileStatus">
             <li title="Usuário Github">
               <ProfileIcon icon={faGithub} />
-              Matheus-Pazinati
+              {profile.login}
             </li>
             <li title="Escola">
               <ProfileIcon icon={faBuilding} />
@@ -44,7 +67,7 @@ export function Profile() {
             </li>
             <li title="Seguidores">
               <ProfileIcon icon={faUserGroup} />
-              30 seguidores
+              {profile.followers} seguidores
             </li>
           </ul>
         </div>
